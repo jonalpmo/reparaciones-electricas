de817be06a7de21fb998217805586c0008507eee
Jonathan Alpizar <jonalpmo@gmail.com>
2018-11-14 23:44:11 -0600

    added more settings

--- content/meta/config.js
+++ content/meta/config.js
@@ -12,8 +12,8 @@ module.exports = {
   authorTwitterAccount: "greglobinski",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "reparaciones electricas",
+  headerSubTitle: "slogan de la empresa",
 
   /* manifest.json */
   manifestName: "HeroBlog - a GatsbyJS starter",
